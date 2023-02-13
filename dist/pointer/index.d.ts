75fb773f8c9efd4ec52c3d5940f5057f377a2cb9
tochibedford <musicofbyte@gmail.com>
2023-02-13 13:07:24 +0100

    chore: compiled refactor

--- dist/pointer/index.d.ts
+++ dist/pointer/index.d.ts
@@ -18,6 +18,46 @@ declare class Character implements TCharacter {
     update: () => void;
     constructor(x: number, y: number, dx: number, dy: number, rotation: number, character: string, drag: number, focusPoint: focusPoint, size: number, color: string, canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, pointer: PointerObject);
 }
+declare class ImageCharacter implements TImageCharacter {
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+    rotation: number;
+    src: HTMLImageElement;
+    drag: number;
+    size: number;
+    focusPoint: focusPoint;
+    pointer: PointerObject;
+    draw: () => void;
+    update: () => void;
+    constructor(x: number, y: number, dx: number, dy: number, rotation: number, src: HTMLImageElement, drag: number, focusPoint: focusPoint, size: number, canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, pointer: PointerObject);
+}
+/**
+ * @param {number} x - x coordinate
+ * @param {number} y - y coordinate
+ * @param {number} dx - x velocity
+ * @param {number} dy - y velocity
+ * @param {number} rotation - rotation of element
+ * @param {HTMLElement} element - element to be displayed
+ * @param {number} drag - drag of element
+ * @param {focusPoint} focusPoint - focus point of element
+ * @param {PointerObject} pointer - pointer object
+ */
+declare class ElementCharacter implements TElementCharacter {
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+    rotation: number;
+    element: HTMLElement;
+    drag: number;
+    focusPoint: focusPoint;
+    pointer: PointerObject;
+    draw: () => void;
+    update: () => void;
+    constructor(x: number, y: number, dx: number, dy: number, rotation: number, element: HTMLElement, drag: number, focusPoint: focusPoint, pointer: PointerObject);
+}
 declare function init(objects: (TCharacter | TImageCharacter | TElementCharacter)[], pointer: PointerObject, element?: HTMLElement): void;
 declare function init(objects: (TCharacter | TImageCharacter | TElementCharacter)[], pointer: PointerObject, canvas?: HTMLCanvasElement, context?: CanvasRenderingContext2D): void;
 /**
@@ -27,5 +67,5 @@ declare function init(objects: (TCharacter | TImageCharacter | TElementCharacter
  * @param pointer this is the pointer object that the Character is bound to
  */
 declare function animate(objectChar: TCharacter | TImageCharacter | TElementCharacter): void;
-export { animate, init, Character, TImageCharacter };
+export { animate, init, Character, ImageCharacter, ElementCharacter };
 //# sourceMappingURL=index.d.ts.map
